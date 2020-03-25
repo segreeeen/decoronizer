0dcd3ba7a81738dfdf27baf55c757fad6d59bb16
Felix Batusic <k1455064@students.jku.at>
2020-03-25 19:17:12 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,19 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+//    Copyright (C) 2020 Boris Baumann, Felix Batusic
+
+//    This file is part of DeCoronizer.
+//
+//    DeCoronizer is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    DeCoronizer is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
 'use strict';
 
